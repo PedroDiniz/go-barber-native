1bb60461d715d64a3886d2e539bf3532ec97bffd
PedroDiniz <pedro_nork@hotmail.com>
2019-07-30 14:20:48 -0300

    button/input/bg components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
-
-// import { Container } from './styles';
+import Routes from './routes';
 
 export default function src() {
-  return (
-    <View>
-      <Text>Hello</Text>
-    </View>
-  );
+  return <Routes />;
 }
